9f87eefec2999a8a1424a75f6843941296e190c7
sumtwelve <sumtwelve@gmail.com>
2023-04-30 21:29:15 -0600

    more file structure, make server

---
+++ client/src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/style.css';
+import Avatar from '../images/sumtwelve/sumtwelve-avatar-web.jpg';
+
+document.getElementById('avatar-img').src = Avatar;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,40 +3,40 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'Webpack Plugin'
-        }),
-        new MiniCssExtractPlugin()
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.m?js$/i,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+  },
 };
