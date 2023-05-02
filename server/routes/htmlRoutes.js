c92e6c9013bfe2a32a355f5c5a7edc1f7600f165
sumtwelve <sumtwelve@gmail.com>
2023-05-02 16:41:52 -0600

    add html routes for subpages

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,4 +4,22 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
+  app.get('/about', function (req, res) {
+    res.sendFile('../../client/src/html/about.html');
+  });
+  app.get('/contact', function (req, res) {
+    res.sendFile('../../client/src/html/contact.html');
+  });
+  app.get('/games', function (req, res) {
+    res.sendFile('../../client/src/html/games.html');
+  });
+  app.get('/music', function (req, res) {
+    res.sendFile('../../client/src/html/Music.html');
+  });
+  app.get('/resume', function (req, res) {
+    res.sendFile('../../client/src/html/resume.html');
+  });
+  app.get('/web-dev', function (req, res) {
+    res.sendFile('../../client/src/html/web-dev.html');
+  });
 };
