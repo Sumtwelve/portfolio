ad9d7bf0e95c0138224fb95f1dd13ed68d4aae35
sumtwelve <sumtwelve@gmail.com>
2023-05-02 16:49:24 -0600

    revise html routes for subpages

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -5,21 +5,21 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
   app.get('/about', function (req, res) {
-    res.sendFile('../../client/src/html/about.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/about.html'));
   });
   app.get('/contact', function (req, res) {
-    res.sendFile('../../client/src/html/contact.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/contact.html'));
   });
   app.get('/games', function (req, res) {
-    res.sendFile('../../client/src/html/games.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/games.html'));
   });
   app.get('/music', function (req, res) {
-    res.sendFile('../../client/src/html/Music.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/music.html'));
   });
   app.get('/resume', function (req, res) {
-    res.sendFile('../../client/src/html/resume.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/resume.html'));
   });
   app.get('/web-dev', function (req, res) {
-    res.sendFile('../../client/src/html/web-dev.html');
+    res.sendFile(path.join(__dirname, '../client/src/html/web-dev.html'));
   });
 };
