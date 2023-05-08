b2c6e38bff5e899c1a1cb02c643764d7c8a966cd
sumtwelve <sumtwelve@gmail.com>
2023-05-07 22:19:12 -0600

    restructure, refine styling

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,14 @@ allButtons.forEach((button) => button.addEventListener("click", openSection));
 function openSection(e) {
     const btnBackground = getComputedStyle(e.target)['background-color'];
     console.log(btnBackground);
-    if(body.style.backgroundColor === btnBackground) {
-        body.style.backgroundColor = '#ebebeb';
-    } else {
+    if(body.style.backgroundColor !== btnBackground) {
+        // body bg color is not same as button bg color,
+        // meaning tab is not open, so we 'open' the tab
         body.style.backgroundColor = btnBackground;
+    } else {
+        // body bg color is same as button just clicked,
+        // which means that tab is currently 'open',
+        // so we 'close' the tab (set bg color to its default)
+        body.style.backgroundColor = '#ebebeb';
     }
 }
