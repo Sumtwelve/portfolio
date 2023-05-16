00d5455bd365d15a9879e5d3f65fbd1e6cc05f57
sumtwelve <sumtwelve@gmail.com>
2023-05-16 02:47:38 -0600

    finish contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,6 +8,15 @@ const Contact = () => {
     return (
         <main className='subpage-main contact-main'>
             <h2>Contact</h2>
+            <h3>
+                For employment inquiries, the best way to contact me is
+                through <a href="https://www.linkedin.com/in/noah-nielsen/" target="_blank" rel="noreferrer">LinkedIn.</a> I
+                look forward to talking with you!
+            </h3>
+            <p>
+                For all other inquiries, shoot me an email
+                at<br/><strong>sumtwelve@gmail.com</strong>
+            </p>
             <BackBtn />
         </main>
     )
